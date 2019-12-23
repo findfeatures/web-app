d469427a2a6d2d8d3a87e95f613b4996307695eb
Calum <calum@paceup.com>
2019-12-23 20:31:41 +0000

    added fix for jwt in local storage

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,22 +3,27 @@ import withSizes from "react-sizes";
 import PropTypes from "prop-types";
 import UnsupportedError from "../../components/UnsupportedError";
 import Login from "../Login";
-import { Redirect } from "@reach/router";
+import { Redirect, navigate } from "@reach/router";
 import { StyledRouter } from "./Main.Style.js";
 import Dashboard from "../Dashboard";
+import { connect } from "react-redux";
 
 class Main extends React.Component {
 	render() {
 		const isSupportedScreenSize = this.props.isSupportedScreenSize;
 
-		const jwt_token = localStorage.getItem("JWT_TOKEN");
+		const jwt_token = this.props.authData.JWT;
+
+		if (!jwt_token) {
+			navigate('/login')
+		}
 
 		return (
 			<>
 				<StyledRouter>
 					<Redirect from="/" to="/login" noThrow />
-					<Login path="/login" />
-					{jwt_token ? <Dashboard path="/dashboard" /> : null}
+					<Login path="/login"/>
+					<Dashboard path="/dashboard" />
 				</StyledRouter>
 				<UnsupportedError show={!isSupportedScreenSize} />
 			</>
@@ -28,10 +33,23 @@ class Main extends React.Component {
 
 Main.propTypes = {
 	isSupportedScreenSize: PropTypes.bool,
+	authData: PropTypes.shape({
+		JWT: PropTypes.string
+	}),
 };
 
 const mapSizesToProps = ({ width, height }) => ({
 	isSupportedScreenSize: width > 1000 && height > 600,
 });
 
-export default withSizes(mapSizesToProps)(Main);
+const mapStateToProps = (reduxState) => {
+	return {
+		authData: reduxState.auth.data,
+	}
+};
+
+export default connect(
+	mapStateToProps,
+	{},
+)(withSizes(mapSizesToProps)(Main));
+
