f6b4154c750aa38076dbf4c37e3095487a54135a
Calum <calum@paceup.com>
2020-01-01 17:23:30 +0000

    updated api to use v1/

--- src/api.js
+++ src/api.js
@@ -2,21 +2,21 @@ export const BASE_URL =
 	process.env.REACT_APP_BASE_URL || "http://localhost:8000";
 
 export const auth_user_api = {
-	url: `${BASE_URL}/user/auth`,
+	url: `${BASE_URL}/v1/user/auth`,
 	method: "POST",
 };
 
 export const check_user_api = {
-	url: `${BASE_URL}/user`,
+	url: `${BASE_URL}/v1/user`,
 	method: "HEAD",
 };
 
 export const sign_up_user_api = {
-	url: `${BASE_URL}/user`,
+	url: `${BASE_URL}/v1/user`,
 	method: "POST",
 };
 
 export const verify_email_user_api = {
-	url: `${BASE_URL}/user-token`,
+	url: `${BASE_URL}/v1/user-token`,
 	method: "POST",
 };
